95455fcb378e1cdcfd8fd1113578bd748d26326f
Icaro-Lima <icaro.lima@ccc.ufcg.edu.br>
2018-04-28 01:12:09 -0300

    Agora remove e exibe mensagem de erro caso dê algum erro na hora de publicar

--- front.js
+++ front.js
@@ -1,12 +1,22 @@
 const table = document.getElementById('table');
 
+const frontEnd = 'icaro';
+const credentials = 'icaro:amendoboboyeah';
+
 let msgs = [];
 function draw() {
-  let html = '<thead class="thead-dark"><tr><th scope="row">#</th><th scope="col">Autor</th><th scope="col">Titulo</th><th scope="col">Mensagem</th></tr></thead>';
-  html += "<tbody>";
-  html += msgs.map(msg => `<tr><th scope="row">${msg.id}</th><td>${msg.author}</td><td>${msg.title}</td><td>${msg.msg}</td></tr>`).join();
-  html += '<tr><th scope="row">+</th><td><input id="author" type="text"/></td><td><input id="title" type="text"/></td><td><input id="msg" type="text"/></td></tr></tbody>';
-
+  let html = '<thead class="thead-dark"><tr><th scope="row">#</th><th scope="col">Autor</th><th scope="col">Titulo</th><th scope="col">Mensagem</th><th score="col"></th></tr></thead>';
+  html += '<tbody><tr><th scope="row">+</th><td><input id="author" type="text"/></td><td><input id="title" type="text"/></td><td><input id="msg" type="text"/></td><td><button type="button" class="btn btn-success" onclick="submit()">Publicar</button></td></tr>';
+  for (let i = msgs.length - 1; i >= 0; i--) {
+    let msg = msgs[i];
+    html += `<tr><th scope="row">${msg.id}</th><td>${msg.author}</td><td>${msg.title}</td><td>${msg.msg}</td>`;
+    if (msg.frontend === frontEnd) {
+      html += `<td><button type="button" id="${msg.id}" class="btn btn-dark" onclick="remove(this)">Remover</button></td></tr>`;
+    } else {
+      html += `<td><button type="button" id="${msg.id}" class="btn btn-dark" onclick="remove(this)" disabled>Remover</button></td></tr>`;
+    }
+  }
+  html += '</tbody>';
   table.innerHTML = html;
 }
 
@@ -21,15 +31,38 @@ function submit() {
       author: inputAuthor.value,
       title: inputTitle.value,
       msg: inputMsg.value,
-      credentials: 'icaro:amendoboboyeah'
+      credentials: credentials
     }),
-  }).then(response => response.json()).then(function(response) {
-    msgs.push({ id: response.id, author: inputAuthor.value, title: inputTitle.value, msg: inputMsg.value });
-    draw();
+  }).then(function (response) {
+    return response.json();
+  }).then(function(response) {
+    if (response.id) {
+      msgs.push({ id: response.id, author: inputAuthor.value, title: inputTitle.value, msg: inputMsg.value, frontend: frontEnd });
+      draw();
 
-    inputAuthor.value = '';
-    inputTitle.value = '';
-    inputMsg.value = '';
+      inputAuthor.value = '';
+      inputTitle.value = '';
+      inputMsg.value = '';
+    } else {
+      alert(response.message);
+    }
+  });
+}
+
+function remove(butt) {
+  fetch('http://150.165.85.16:9900/api/msgs/' + butt.id, {
+    method: 'DELETE',
+    body: JSON.stringify({
+      credentials: credentials
+    })
+  }).then(function () {
+    for (let i = 0; i < msgs.length; i++) {
+      if (msgs[i].id == butt.id) {
+        msgs.splice(i, 1);
+      }
+    }
+  }).then(function () {
+    draw();
   });
 }
 
