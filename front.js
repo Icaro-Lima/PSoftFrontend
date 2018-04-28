2d8f2e22e9b8868c60590b229e1e9b844bce47b5
Icaro-Lima <icaro.lima@ccc.ufcg.edu.br>
2018-04-27 23:42:01 -0300

    Adicionando .gitignore.

--- front.js
+++ front.js
@@ -2,9 +2,10 @@ const table = document.getElementById('table');
 
 let msgs = [];
 function draw() {
-  let html = '<thead><tr><th>Autor</th><th>Titulo</th><th>Mensagem</th></tr></thead>';
-  html += msgs.map(msg => `<tbody><tr><td>${msg.author}</td><td>${msg.title}</td><td>${msg.msg}</td></tr>`).join();
-  html += '<tr><td><input id="author" type="text"/></td><td><input id="title" type="text"/></td><td><input id="msg" type="text"/></td></tr></tbody>';
+  let html = '<thead class="thead-dark"><tr><th scope="row">#</th><th scope="col">Autor</th><th scope="col">Titulo</th><th scope="col">Mensagem</th></tr></thead>';
+  html += "<tbody>";
+  html += msgs.map(msg => `<tr><th scope="row">${msg.id}</th><td>${msg.author}</td><td>${msg.title}</td><td>${msg.msg}</td></tr>`).join();
+  html += '<tr><th scope="row">+</th><td><input id="author" type="text"/></td><td><input id="title" type="text"/></td><td><input id="msg" type="text"/></td></tr></tbody>';
 
   table.innerHTML = html;
 }
@@ -22,8 +23,8 @@ function submit() {
       msg: inputMsg.value,
       credentials: 'icaro:amendoboboyeah'
     }),
-  }).then(function(response) {
-    msgs.push({ author: inputAuthor.value, title: inputTitle.value, msg: inputMsg.value });
+  }).then(response => response.json()).then(function(response) {
+    msgs.push({ id: response.id, author: inputAuthor.value, title: inputTitle.value, msg: inputMsg.value });
     draw();
 
     inputAuthor.value = '';
