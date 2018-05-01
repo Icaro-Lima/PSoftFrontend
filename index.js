71047268a05d9b7a279a2823829db7ff42f1dd85
Icaro-Lima <icaro.lima@ccc.ufcg.edu.br>
2018-05-01 01:30:03 -0300

    Adicionados método de postar mensagens e de deletar mensagens, também foram adicionadas algumas animações

--- index.js
+++ index.js
@@ -1,72 +1,115 @@
-const table = document.getElementById('table');
+const board = document.getElementById('board');
+const editableBoard = document.getElementById('editable-board');
+const trashBin = document.getElementById('trash-bin');
 
-const frontEnd = 'icaro';
-const credentials = 'icaro:amendoboboyeah';
+const backendUrl = 'http://150.165.85.16:9900/';
 
-let msgs = [];
-function draw() {
-  let html = '<thead class="thead-dark"><tr><th scope="row">#</th><th scope="col">Autor</th><th scope="col">Titulo</th><th scope="col">Mensagem</th><th score="col"></th></tr></thead>';
-  html += '<tbody>';
-  for (let i = msgs.length - 1; i >= 0; i--) {
-    let msg = msgs[i];
-    html += `<tr><th scope="row">${msg.id}</th><td>${msg.author}</td><td>${msg.title}</td><td>${msg.msg}</td>`;
-    if (msg.frontend === frontEnd) {
-      html += `<td><button type="button" id="${msg.id}" class="btn btn-dark" onclick="remove(this)">Remover</button></td></tr>`;
-    } else {
-      html += `<td><button type="button" id="${msg.id}" class="btn btn-dark" onclick="remove(this)" disabled>Remover</button></td></tr>`;
-    }
-  }
-  html += '</tbody>';
-  table.innerHTML = html;
+const frontend = 'icaro';
+const secret = 'amendoboboyeah';
+
+const colors = [ '#00FFFF', '#00CED1', '#40E0D0', '#48D1CC', '#20B2AA', '#008B8B', '#008B8B', '#7FFFD4', '#66CDAA', '#5F9EA0' ];
+
+function beginBoardLoad() {
+  fetch(backendUrl + 'api/msgs').then(function (response) {
+    return response.json();
+  }).then(function (msgs) {
+    endLoadBoard(msgs);
+  });
 }
 
-function submit() {
-  let inputMsg = document.getElementById('msg');
-  let inputTitle = document.getElementById('title');
-  let inputAuthor = document.getElementById('author');
+function endLoadBoard(msgs) {
+  msgs.sort(function (left, right) {
+    return moment(left.created_at).diff(moment(right.created_at));
+  });
 
-  fetch('http://150.165.85.16:9900/api/msgs', {
+  msgs.forEach(addMessage);
+}
+
+function addMessage(msg) {
+  if (msg.color === undefined) {
+    msg.color = colors[Math.floor(Math.random() * colors.length)];
+  }
+
+  board.innerHTML = `<div id="msg_${msg.id}" class="message"` + (msg.frontend === frontend ? `ondragstart="beginDrag(event)" draggable="true" ondragend="onDragEnd(event)"` : ``) + ` style="background-color: ${msg.color}">
+    <h4 align="left" class="text-capitalize">${msg.title}</h4>
+    <p align="left" class="font-weight-light">${msg.msg}</p>
+    <p align="left" class="who">${msg.author}, há ${moment(msg.created_at).fromNow()}</p>
+  </div>` + board.innerHTML;
+}
+
+function addMessageForm() {
+  let color = colors[Math.floor(Math.random() * colors.length)];
+  editableBoard.innerHTML = `<div class="editable-message" style="background-color: ${color}">
+    <h4 id="editable-title" contenteditable="true" align="left" class="text-capitalize">Título</h4>
+    <p id="editable-msg" contenteditable="true" align="left" class="font-weight-light">Mensagem</p>
+    <p id="editable-author" contenteditable="true" align="left" class="who">Autor</p>
+    <p><button type="button" class="btn btn-primary" onClick="beginSubmit()">Postar</button>
+    <button type="button" class="btn btn-danger" onClick="discart()">Descartar</button></p>
+  </div>`;
+
+  editableBoard.getElementsByClassName('editable-message')[0].color = color;
+
+  window.scrollTo(0, 0);
+}
+
+function beginSubmit() {
+  let color = editableBoard.getElementsByClassName('editable-message')[0].color;
+  let title = document.getElementById('editable-title').innerText;
+  let msg = document.getElementById('editable-msg').innerText;
+  let author = document.getElementById('editable-author').innerText;
+
+  fetch(backendUrl + 'api/msgs', {
     method: 'POST',
     body: JSON.stringify({
-      author: inputAuthor.value,
-      title: inputTitle.value,
-      msg: inputMsg.value,
-      credentials: credentials
-    }),
-  }).then(function (response) {
-    return response.json();
-  }).then(function(response) {
-    if (response.id) {
-      msgs.push({ id: response.id, author: inputAuthor.value, title: inputTitle.value, msg: inputMsg.value, frontend: frontEnd });
-      draw();
-
-      inputAuthor.value = '';
-      inputTitle.value = '';
-      inputMsg.value = '';
-    } else {
-      alert(response.message);
-    }
+      title: title,
+      msg: msg,
+      author: author,
+      credentials: frontend + ':' + secret
+    })
+  }).then(response => response.json()).then(function (response) {
+    editableBoard.innerHTML = '';
+    response.color = color;
+    addMessage(response);
   });
 }
 
-function remove(butt) {
-  fetch('http://150.165.85.16:9900/api/msgs/' + butt.id, {
+function discart() {
+  editableBoard.innerHTML = '';
+}
+
+function beginDrag(event) {
+  event.dataTransfer.setData("text", event.target.id.split('_')[1]);
+  event.dropEffect = "move";
+  trashBin.setAttribute('drop', true);
+}
+
+function onAllowDrop(event) {
+  event.preventDefault();
+  event.dataTransfer.dropEffect = "move";
+}
+
+function onDrop(event) {
+  trashBin.setAttribute('drop', false);
+
+  let id = event.dataTransfer.getData("text");
+
+  fetch(backendUrl + 'api/msgs/' + id, {
     method: 'DELETE',
     body: JSON.stringify({
-      credentials: credentials
+      credentials: frontend + ':' + secret
     })
-  }).then(function () {
-    for (let i = 0; i < msgs.length; i++) {
-      if (msgs[i].id == butt.id) {
-        msgs.splice(i, 1);
-      }
+  }).then(function (response) {
+    if (response.status === 200) {
+      let element = document.getElementById('msg_' + id);
+      board.removeChild(element);
     }
-  }).then(function () {
-    draw();
   });
 }
 
-fetch("http://150.165.85.16:9900/api/msgs").then(response => response.json()).then(function(json) {
-  msgs = json;
-  draw();
-});
+function onDragEnd(event) {
+  trashBin.setAttribute('drop', false);
+}
+
+moment.locale('pt-BR');
+
+beginBoardLoad();
